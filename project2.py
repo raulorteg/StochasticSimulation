c85d4aa4bd17fbce71d233d3fbfcaf77ef2db808
Raul <s202489@student.dtu.dk>
2021-06-21 21:25:34 +0200

    base case pandemia :bookmark:

--- project2.py
+++ project2.py
@@ -161,15 +161,6 @@ if __name__ == "__main__":
 	plt.legend()
 	plt.show()
 
-	year = [1950, 1960, 1970, 1980, 1990, 2000, 2010, 2018]
-	population_by_continent = {
-    'africa': [228, 284, 365, 477, 631, 814, 1044, 1275],
-    'americas': [340, 425, 519, 619, 727, 840, 943, 1006],
-    'asia': [1394, 1686, 2120, 2625, 3202, 3714, 4169, 4560],
-    'europe': [220, 253, 276, 295, 310, 303, 294, 293],
-    'oceania': [12, 15, 19, 22, 26, 31, 36, 39],
-	}
-
 	time = list(range(len(S_history)))
 	distribution_stages = {
     'Susceptible': S_history,
@@ -186,6 +177,3 @@ if __name__ == "__main__":
 	ax.set_xlabel('Year')
 	ax.set_ylabel('Number of people (millions)')
 	plt.show()
-
-
-
